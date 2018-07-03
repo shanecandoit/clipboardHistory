aeb5cb4657965731dd5c78861afbbd4dd5e6b5f2
Shane Page <shanepage@ku.edu>
2018-07-03 10:02:19 -0500

    upgrade to python3. it doesnt work, but now it runs.

--- clip_history.py
+++ clip_history.py
@@ -1,4 +1,5 @@
-from Tkinter import *
+#from Tkinter import *
+import tkinter
 
 '''shane 2012 gpl3 or greater'''
 
@@ -6,15 +7,16 @@ class Board(object):
     '''a text widget that sits on top of other windows and holds your clipboard
     history'''
     def __init__(self):
-        self.root = Tk()
+        self.root = tkinter.Tk()
         self.root.title('Clip History')
         self.root.wm_attributes('-topmost', 1)
-        self.tx=Text(self.root)
+        self.tx = tkinter.Text(self.root)
         self.tx.configure(font=('new courier',9),width=30,height=15)
         self.tx.pack(fill='both',expand=1)
         
         self.cont=''
         self.getClip()
+        #self.loop()
         
     def getClip(self):
         #print 'get clip'
@@ -23,7 +25,7 @@ class Board(object):
         except:
             pass
         if not self.cont == self.clip:
-            #print 'new clip text'
+            print('new clip text', self.clip)
             self.cont=self.clip
             try:
                 self.tx.insert(END,'\n'+self.cont)
@@ -31,5 +33,8 @@ class Board(object):
                 pass
         self.root.after(1000,self.getClip)
 
+    #def loop(self):
+        #tkinter.mainloop()
+
 b=Board()
-mainloop()
+tkinter.mainloop()
